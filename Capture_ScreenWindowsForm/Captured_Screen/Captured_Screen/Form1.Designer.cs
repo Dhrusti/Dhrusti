c3f963a7978fe1322ea5fc4221bccf1a11039ada
Dhrusti Suthar <dhrusti.suthar@reynasolutions.com>
2023-04-19 10:52:28 +0530

    commit captureScreenshotPOC

---
+++ Capture_ScreenWindowsForm/Captured_Screen/Captured_Screen/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Captured_Screen
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.textImage = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.button2 = new System.Windows.Forms.Button();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(404, 175);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(186, 66);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Captured_Screen";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textImage
+			// 
+			this.textImage.AutoSize = true;
+			this.textImage.Location = new System.Drawing.Point(29, 25);
+			this.textImage.Name = "textImage";
+			this.textImage.Size = new System.Drawing.Size(39, 13);
+			this.textImage.TabIndex = 1;
+			this.textImage.Text = "Label1";
+			this.textImage.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(162, 18);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(459, 20);
+			this.textBox1.TabIndex = 2;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(162, 64);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(459, 20);
+			this.textBox2.TabIndex = 3;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackColor = System.Drawing.SystemColors.WindowText;
+			this.pictureBox1.Location = new System.Drawing.Point(32, 143);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(133, 98);
+			this.pictureBox1.TabIndex = 4;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.textBox1);
+			this.panel1.Controls.Add(this.button1);
+			this.panel1.Controls.Add(this.pictureBox1);
+			this.panel1.Controls.Add(this.textBox2);
+			this.panel1.Controls.Add(this.textImage);
+			this.panel1.Location = new System.Drawing.Point(72, 12);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(650, 271);
+			this.panel1.TabIndex = 5;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(476, 344);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(186, 66);
+			this.button2.TabIndex = 0;
+			this.button2.Text = "Save as Image";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(150, 344);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(320, 20);
+			this.textBox3.TabIndex = 2;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(84, 347);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(41, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Name :";
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label textImage;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Label label1;
+	}
+}
+
---
+++ Capture_ScreenWindowsForm/Captured_Screen/Captured_Screen/Form2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Captured_Screen
+{
+	public partial class Form2 : Form
+	{
+		public Form2()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			//for (int i = 0; i < 5; i++)
+			while (true) 
+			{
+				Thread.Sleep(2000);
+				CaptureAtDefinedIntervals();
+			}
+		}
+
+		public void CaptureAtDefinedIntervals()
+		{
+			Bitmap screenshot = new Bitmap(SystemInformation.VirtualScreen.Width,
+										   SystemInformation.VirtualScreen.Height,
+										   PixelFormat.Format32bppArgb);
+			Graphics screenGraph = Graphics.FromImage(screenshot);
+			screenGraph.CopyFromScreen(SystemInformation.VirtualScreen.X,
+									   SystemInformation.VirtualScreen.Y,
+									   0,
+									   0,
+									   SystemInformation.VirtualScreen.Size,
+									   CopyPixelOperation.SourceCopy);
+
+			screenshot.Save(DateTime.Now.ToString("ddMMyyyyhhmmss") + ".png", System.Drawing.Imaging.ImageFormat.Png);
+			//screenshot.Save("Screenshot.png", System.Drawing.Imaging.ImageFormat.Png);
+		}
+	}
+}
